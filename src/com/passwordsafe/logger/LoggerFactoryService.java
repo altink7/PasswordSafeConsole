871f6b379a6f8b81242875f4426f01df9563cd0f
Altin <kelmendialtin48@gmail.com>
2022-11-26 19:14:50 +0100

    hand in 01 features implemented

---
+++ src/com/passwordsafe/logger/LoggerFactoryService.java
@@ -0,0 +1,36 @@
+package com.passwordsafe.logger;
+
+/***
+ * hier werden die Methoden von LoggerRepoService implementiert (Service Layer)
+ * + hier wird Singleton Pattern angewendet
+ *
+ */
+
+public class LoggerFactoryService implements LoggerRepoFactory{
+    private static final Object object = new Object();
+    private static volatile LoggerFactoryService instance;
+
+    private final LoggerRepo logger;
+
+    private LoggerFactoryService(){
+        this.logger = new LoggerService();
+    }
+
+    public static LoggerFactoryService getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+
+        synchronized (object) {
+            if (instance == null) {
+                instance = new LoggerFactoryService();
+            }
+            return instance;
+        }
+    }
+
+    @Override
+    public LoggerRepo loggerRepoInstance() {
+        return logger;
+    }
+}
